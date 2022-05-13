ec1032e1c6e4c6bcf4d0bd9c4cf2fa7de829675d
jin5437 <1327003606@qq.com>
2022-05-13 19:08:04 +0800

    add  components

--- my-vue2-music/src/components/navbar/NavBar.vue
+++ my-vue2-music/src/components/navbar/NavBar.vue
@@ -1,13 +1,80 @@
 <template>
-  <div></div>
+  <div class="navBarContainer">
+    <div class="navBar">
+      <div 
+        class="barItem"
+        :class="index == activeNum ? 'active' : ''"
+        v-for="(item,index) in navBarItem"
+        :key="index"
+        @click="clickBarItem(item.path,index)"
+      >
+        {{item.name}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'NavBar'
+  name:'NavBar',
+  data() {
+    return {
+      activeNum:0,
+    }
+  },
+  props:{
+    navBarItem: {
+      
+    }
+  },
+  methods: {
+    clickBarItem(path,index){
+      if(this.activeNum == index){
+        return
+      }
+      this.activeNum = index
+      // 将数据发送给父组件Discover
+      this.$emit("clickBarItem",path)
+    }
+  },
+    
+  
 }
 </script>
 
-<style>
+<style scoped>
+.navBarContainer {
+  height: 60px;
+  width: 100%;
+  position: relative;
+}
 
+.navBar {
+  display: flex;
+  position: relative;
+  left: 0px;
+  top: 0;
+  background-color: #fff;
+  z-index: 100;
+  width: 100%;
+  color: rgb(49, 49, 49);
+  align-items: center;
+  padding: 10px 0 20px 20px;
+}
+
+.barItem {
+  font-size: 13px;
+  margin: 0 10px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.active {
+  font-size: 16px;
+  color: black;
+  font-weight: 600;
+  padding-bottom: 5px;
+  border-bottom: 3px solid #ec4141;
+  transform: translateY(4px);
+}  
 </style>
--- my-vue2-music/src/views/discover/Discover.vue
+++ my-vue2-music/src/views/discover/Discover.vue
@@ -1,21 +1,53 @@
 <template>
   <div>
-    <nav-bar></nav-bar>
+    <nav-bar @click="clickBarItem" :navBarItem="navBarData"></nav-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import NavBar from '../../components/navbar/NavBar.vue'
+import NavBar from "../../components/navbar/NavBar.vue";
 export default {
-  name:'Discover',
-  components:[
-    NavBar
-  ]
-
-}
+  name: "Discover",
+  components: { NavBar },
+  data() {
+    return {
+      navBarData: [
+        {
+          name: "歌单",
+          path: "./discover/musiclist",
+        },
+        {
+          name: "歌手",
+          path: "./discover/singer",
+        },
+        {
+          name: "排行榜",
+          path: "./discover/rangking",
+        }
+      ],
+    };
+  },
+  methods: {
+    // 接收子组件navbar传过来的点击事件，为什么要接收呢？
+    clickBarItem(path){
+      
+    }
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.discoverContainer {
+  overflow: hidden;
+  padding: 0 !important;
+}
 
+.discover {
+  /* max-width: 1000px; */
+  margin: auto;
+  overflow: scroll;
+  height: calc(80vh - 157px);
+  padding: 0 15px;
+}
 </style>
